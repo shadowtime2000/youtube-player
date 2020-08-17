06c5b0203760ad9c2e83c1b03b7a0e28b3fbaf63
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-08-17 10:39:44 -0700

    Code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
+const { Youtube } = require("vue-youtube");
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <input v-model="video" placeholder="YouTube Link Here" class="form-control" /><br/>
+    <youtube v-bind:video-id="vd" ref="youtube"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      video: "",
+    }
+  },
+  computed: {
+    vd() {
+      return this.$youtube.getIdFromUrl(this.video);
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: red;
 }
 </style>
